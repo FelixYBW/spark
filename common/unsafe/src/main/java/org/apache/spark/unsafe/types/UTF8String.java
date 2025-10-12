bfc9fee5454a234a87afd0c211470ba74fcab4bd
felix <binwei.yang@ibm.com>
2025-10-12 19:02:55 +0000

    bypass array_eaula

--- common/unsafe/src/main/java/org/apache/spark/unsafe/types/UTF8String.java
+++ common/unsafe/src/main/java/org/apache/spark/unsafe/types/UTF8String.java
@@ -771,8 +771,8 @@ public final class UTF8String implements Comparable<UTF8String>, Externalizable,
         return -1;
       }
       if (v.numBytes == 1 && numBytes > 100){
-	  if (ByteArrayMethods.arrayEquals(base, offset + i, v.base, v.offset, v.numBytes, true)) {
-	    return -1;
+	  if (false /*&& ByteArrayMethods.arrayEquals(base, offset + i, v.base, v.offset, v.numBytes, true)*/) {
+	    return c;
 	  }
       } else {
 	  if (ByteArrayMethods.arrayEquals(base, offset + i, v.base, v.offset, v.numBytes)) {
