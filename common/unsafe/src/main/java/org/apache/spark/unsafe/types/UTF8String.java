8a01ed64d79a10e943ec7f77d957a1731143332b
felix <binwei.yang@ibm.com>
2025-10-14 03:15:09 +0000

    count arrayEqual call time

--- common/unsafe/src/main/java/org/apache/spark/unsafe/types/UTF8String.java
+++ common/unsafe/src/main/java/org/apache/spark/unsafe/types/UTF8String.java
@@ -753,6 +753,7 @@ public final class UTF8String implements Comparable<UTF8String>, Externalizable,
    * @param start the start position of the current string for searching
    * @return the position of the first occurrence of substr, if not found, -1 returned.
    */
+  private static int callCount = 0;
   public int indexOf(UTF8String v, int start) {
     if (v.numBytes() == 0) {
       return 0;
@@ -770,14 +771,20 @@ public final class UTF8String implements Comparable<UTF8String>, Externalizable,
         return -1;
       }
       if (v.numBytes == 1 && numBytes > 100){
-        for (int lp=0; lp<100; lp++){
-	  if (ByteArrayMethods.arrayEquals(base, offset + i, v.base, v.offset, v.numBytes, true)) {
-	    return -1;
-	  }
-	}
-	return -1;
+        if (callCount < 10000){
+          long startTime = System.nanoTime();
+          for (int lp=0; lp<10000; lp++){
+            if (ByteArrayMethods.arrayEquals(base, offset + i, v.base, v.offset, v.numBytes, true)) {
+              return -1;
+            }
+          }
+          long endTime = System.nanoTime();
+          System.out.println("10000 call elapsed time " + (endTime - startTime) + " ns " + " per call " + ((endTime - startTime)/10000) + " ns");
+          callCount += 1;
+        }
+        return -1;
       } else {
-	if (ByteArrayMethods.arrayEquals(base, offset + i, v.base, v.offset, v.numBytes)) {
+	      if (ByteArrayMethods.arrayEquals(base, offset + i, v.base, v.offset, v.numBytes)) {
           return c;
         }
       }
